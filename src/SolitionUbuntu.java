83bc530c216f625cd86fd6b6dfe0f2f7e0955584
Pheonix in Ubuntu <15927328287@163.com>
2018-12-12 16:45:58 +0800

    2018.Dev.12: Pheonix changes in Ubuntu.

---
+++ src/SolitionUbuntu.java
@@ -0,0 +1,491 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Stack;
+import java.util.*;
+/**
+ * @ Author     ：Pheonix
+ * @ Date       ：Created in 下午4:31 18年12月12日
+ * @ Description：
+ */
+/**
+ * @ Author     ：Pheonix
+ * @ Date       ：Created in 上午10:30 18年11月14日
+ * @ Description：Leecode 103
+ * @ Modified By：
+ * @ Version    ：1.0
+ */
+public class SolitionUbuntu {
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(3);
+        TreeNode r1 = new TreeNode(9);
+        TreeNode r2 = new TreeNode(20);
+        TreeNode r3 = new TreeNode(15);
+        TreeNode r4 = new TreeNode(7);
+//        TreeNode r5 = new TreeNode(15);
+        root.left = r1;
+        r2.right = r4;
+        r2.left = r3;
+        root.right = r2;
+        SolitionUbuntu solution = new SolitionUbuntu();
+        System.out.println(solution.findDeep(root,0));
+        System.out.println(solution.zigzagLevelOrder(root));
+//        int[] arr = solution.findFrequentTreeSum(root);
+        //List l = solution.zigzagLevelOrder(root);
+//        System.out.print(solution.nthMagicalNumber(1,2,3));
+
+
+    }
+
+    //103
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        List re = new ArrayList();
+        Stack<TreeNode> st1 = new Stack<TreeNode>();
+        Stack<TreeNode> st2 = new Stack<TreeNode>();
+        if(root != null) {
+            if (root.left != null)
+                st1.push(root.left);
+            if (root.right != null)
+                st1.push(root.right);
+            List rr = new ArrayList();
+            rr.add(root.val);
+            re.add(rr);
+            int useL = 1;
+            while (!st1.isEmpty() || !st2.isEmpty()) {
+                List rr2 = new ArrayList();
+                if (useL == 1) {
+                    while (!st1.isEmpty()) {
+                        TreeNode t = (TreeNode) st1.pop();
+                        rr2.add(t.val);
+                        if (t.right != null) {
+                            st2.push(t.right);
+                        }
+                        if (t.left != null) {
+                            st2.push(t.left);
+                        }
+                    }
+                    useL = 0;
+                } else {
+                    while (!st2.isEmpty()) {
+                        TreeNode t = (TreeNode) st2.pop();
+                        rr2.add(t.val);
+                        if (t.left != null) {
+                            st1.push(t.right);
+                        }
+                        if (t.right != null) {
+                            st1.push(t.left);
+                        }
+                    }
+                    useL = 1;
+                }
+                re.add(rr2);
+            }
+        }
+        return re;
+    }
+    public static int findDeep(TreeNode root,int deep){
+        if (root.left == null && root.right == null){
+            return deep;
+        }
+        if(root.left != null){
+            deep = findDeep(root.left, deep+1);
+        }
+        if(root.right != null){
+            deep = findDeep(root.right, deep+1);
+        }
+        return deep;
+    }
+
+    //227
+    public int nthMagicalNumber(int N, int A, int B) {
+        //求最大公约数
+        long a = A>B?A:B;
+        long b = A>B?B:A;
+        long C = a % b;
+        if(C == 0){
+            C = b;
+        }else {
+            while (C != 0) {
+                a = b;
+                b = C;
+                C = a % b;
+            }
+            C = b;
+        }
+        //求遇见一次最小公倍数有几次神奇数字
+        long p = A / C + B / C - 1;
+        long q = N % p;
+        long s = N / p;
+        //求第q个神奇数字
+        long ss = 0;
+        a = A>B?A:B;
+        b = A>B?B:A;
+        if(0 == q){
+            ss = 0;
+        }
+        else if(1 == q){
+            ss = b;
+        }
+        else {
+            long c = a;
+            long d = b;
+            char f = 'd';
+            for (int i = 1; i < q; i++) {
+                if (c > d && c < d + b) {
+                    c = c + a;
+                    f = 'e';
+                } else if (d + b > c && d + b < c + a) {
+                    d = d + b;
+                    f = 'd';
+                } else if (d + b < c) {
+                    d = d + b;
+                    f = 'd';
+                } else if (d + b > c + a) {
+                    c = c + a;
+                    f = 'c';
+                }
+            }
+            if(f == 'e'){
+                ss = c-a;
+            }
+            else if('c' == f){
+                ss = c;
+            }
+            else{
+                ss = d;
+            }
+        }
+        //取模
+        long sss = ss;
+        for(int i = 0;i < s;i++){
+            sss = sss + A / C * B;
+            if(sss > 1000000007)
+                sss = sss % 1000000007;
+        }
+        return (int)sss;
+    }
+
+    //508
+    public int[] findFrequentTreeSum(TreeNode root){
+        ArrayList tlist = new ArrayList();
+
+        //后序遍历树求子树元素和
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        HashMap map = new HashMap();
+        stack.push(root);
+        if (root == null){
+            int[] zx={};
+            return zx;
+        }
+        if(root.left == null && root.right == null)
+        {
+            int[] zx={root.val};
+            return zx;
+        }
+        int value = 0;
+        while(!stack.isEmpty()){
+            TreeNode t = stack.peek();
+
+            if (t.right !=null && !tlist.contains(t.right)){
+                stack.push(t.right);
+            }
+            if (t.left != null && !tlist.contains(t.left)){
+                stack.push(t.left);
+            }
+
+            if(t.left == null&&t.right == null){
+                tlist.add(t);
+
+                if(map.containsKey(t.val)){
+                    map.put(t.val, (int)map.get(t.val)+1);
+                    if((int)map.get(t.val) > value){
+                        value = (int)map.get(t.val);
+                    }
+                }
+                else {
+                    map.put(t.val, 1);
+                    value = value == 0?1 : value;
+                }
+                stack.pop();
+            }
+            else if(t.left == null&&tlist.contains(t.right)){
+                t.val = t.val + t.right.val;
+                tlist.add(t);
+
+                if(map.containsKey(t.val)){
+                    map.put(t.val, (int)map.get(t.val)+1);
+                    if((int)map.get(t.val) > value){
+                        value = (int)map.get(t.val);
+                    }
+                }
+                else {
+                    map.put(t.val, 1);
+                    value = value == 0?1 : value;
+                }
+                stack.pop();
+            }
+            else if(tlist.contains(t.left)&&t.right == null){
+                t.val = t.val + t.left.val;
+                tlist.add(t);
+
+                if(map.containsKey(t.val)){
+                    map.put(t.val, (int)map.get(t.val)+1);
+                    if((int)map.get(t.val) > value){
+                        value = (int)map.get(t.val);
+                    }
+                }
+                else {
+                    map.put(t.val, 1);
+                    value = value == 0?1 : value;
+                }
+                stack.pop();
+            }
+            else if(tlist.contains(t.left)&&tlist.contains(t.right)){
+                t.val = t.val + t.left.val + t.right.val;
+                tlist.add(t);
+
+                if(map.containsKey(t.val)){
+                    map.put(t.val, (int)map.get(t.val)+1);
+                    if((int)map.get(t.val) > value){
+                        value = (int)map.get(t.val);
+                    }
+
+                }
+                else {
+                    map.put(t.val, 1);
+                    value = value == 0?1 : value;
+                }
+                stack.pop();
+            }
+        }
+        //求子树元素和出现次数最多的数
+        List keyList = new ArrayList();
+        for(Object getKey: map.keySet()){
+            if(map.get(getKey).equals(value)){
+                keyList.add(getKey);
+            }
+        }
+        int[] arr = new int[keyList.size()];
+        for(int i=0;i<keyList.size();i++){
+            arr[i] = (int)keyList.get(i);
+        }
+        return arr;
+
+    }
+
+    //872
+    public boolean leafSimilar(TreeNode root1, TreeNode root2) {
+        boolean re = true;
+        Stack<TreeNode> s1 = new Stack<>();
+        Stack<TreeNode> s2 = new Stack<>();
+        if (root1 == null && root2 == null)
+            return true;
+        else if (root1 == null && root2 != null)
+            return false;
+        else if (root1 != null && root2 == null)
+            return false;
+        else {
+            s1.push(root1);
+            s2.push(root2);
+            List lp = new ArrayList();
+            int j = 0;
+            while (!s1.isEmpty()){
+                TreeNode p = s1.pop();
+                if (p.right != null){
+                    s1.push(p.right);
+                }
+                if (p.left != null){
+                    s1.push(p.left);
+                }
+                if(p.left == null && p.right == null){
+                    lp.add(p.val);
+                }
+            }
+            while (!s2.isEmpty()){
+                TreeNode p = s2.pop();
+                if (p.right != null){
+                    s2.push(p.right);
+                }
+                if (p.left != null){
+                    s2.push(p.left);
+                }
+                if(p.left == null && p.right == null){
+                    if(p.val != (int)lp.get(j) || j >= lp.size()){
+                        re = false;
+                        break;
+                    }
+                    j++;
+                }
+            }
+            return re;
+        }
+    }
+
+
+    //709
+    public String toLowerCase(String str) {
+        char[] ch = str.toCharArray();
+        for(int i =0;i<ch.length;i++){
+            if(ch[i] >= 'A'&&ch[i] <= 'Z'){
+                ch[i] = (char)(ch[i] + 32);
+            }
+        }
+        return ch.toString();
+    }
+
+    //461
+    public int hammingDistance(int x, int y) {
+        int res = x ^ y;
+        int resu = 0;
+        while (res!=0){
+            if(res % 2 == 1){
+                resu++;
+            }
+            res = res / 2;
+        }
+        return resu;
+    }
+
+    //771
+    public int numJewelsInStones(String J, String S) {
+        char strJ[] = J.toCharArray();
+        char strS[] = S.toCharArray();
+        int result = 0;
+//        List<Character> listJ= new ArrayList<Character>(Arrays.asList(strJ));
+//        List<Character> listS= new ArrayList<Character>(Arrays.asList(strS));
+        for(int i = 0;i < strS.length;i++){
+            for(int j = 0; j < strJ.length;j++){
+                if(strS[i] == strJ[j]){
+                    result++;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    //237
+    public void deleteNode(ListNode node) {
+        if(node.next!=null) {
+            ListNode p = node.next;
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+
+    //338
+    public int[] countBits(int num) {
+        int [] re = new int[num + 1];
+        if(num>2) {
+            re[0] = 0;
+            re[1] = 1;
+            re[2] = 1;
+            int k = 2;
+            int j = 3;
+            while(j <= num) {
+                if(j == k*2) {
+                    k = k*2;
+                }
+                re[j] = 1 + re[j-k];
+                j++;
+            }
+        }
+        else if (num == 0) {
+            re[0] = 0;
+        }
+        else if (1 == num) {
+            re[0] = 0;
+            re[1] = 1;
+        }
+        else if (2 == num) {
+            re[0] = 0;
+            re[1] = 1;
+            re[2] = 1;
+        }
+        return re;
+    }
+
+    //476
+    public int findComplement(int num){
+        int[] arr = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191, 16383, 32767, 65535,
+                131071, 262143, 524287, 1048575, 2097151, 4194303, 8388607, 16777215, 33554431, 67108863,
+                134217727, 268435455, 536870911, 1073741823, 2147483647};
+        int i = 0;
+        for(;i < 32; i++){
+            if(num <= arr[i])break;
+        }
+        return arr[i] - num;
+    }
+
+    //728
+    public List<Integer> selfDividingNumbers(int left, int right) {
+        List<Integer> res = new ArrayList<Integer>();
+        for(int i = left;i <= right; i++){
+            int j = i;
+            boolean b = true;
+            int k = j%10;
+            while(j/10 != 0){
+                if(k == 0||i % k != 0){
+                    b = false;
+                    break;
+                }
+                j  = j / 10;
+                k = j % 10;
+            }
+            if(k == 0||i % k != 0){
+                b = false;
+            }
+            if(b == true){
+                res.add(i);
+            }
+        }
+        return res;
+    }
+
+    //463
+    public int islandPerimeter(int[][] grid) {
+        int res = 0;
+        for(int i = 0;i < grid.length;i++){
+            for(int j = 0;j < grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    if(i == 0||grid[i - 1][j] == 0)res++;
+                    if(i == grid.length-1||grid[i + 1][j] == 0)res++;
+                    if(j == 0||grid[i][j - 1] == 0)res++;
+                    if(j == grid[i].length-1||grid[i][j + 1] == 0)res++;
+                }
+            }
+        }
+        return res;
+    }
+
+    //390
+//    public int lastRemaining(int n) {
+//        int flag = 1;//->
+//        int i = n;
+//        int j = 0;
+//        while(true){
+//            i = i / 2;
+//            flag = flag == 1? 0 : 1;
+//            j++;
+//        }
+//        if(flag == 1){
+//
+//        }
+//        else{
+//
+//        }
+//    }
+//    public static int[] res(int [] sou, int flag){
+//        int []re = new int [(sou.length)/2];
+//        if(flag == 1) {
+//            for (int i = 0; i < re.length; i++) {
+//                re[i] = sou[2 * i];
+//            }
+//        }
+//        else {
+//            for (int i = 0; i < re.length; i++) {
+//                re[i] = sou[2 * i];
+//            }
+//        }
+//        return re;}
+}
