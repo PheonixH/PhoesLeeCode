e2ef0a3f2e52d69315128d1c970f59c939c87c72
Pheonix in Ubuntu <15927328287@163.com>
2019-04-23 09:30:08 +0800

    94,112,236,617,654,1008,226,104,814,894,589,590,700,559,701,979,230,108

---
+++ src/datestruct/Nodes.java
@@ -0,0 +1,20 @@
+package datestruct;
+
+import java.util.List;
+
+/**
+ * @ Author     ：Pheonix
+ * @ Date       ：Created in 下午3:18 19-4-22
+ * @ Description：
+ */
+public class Nodes {
+    public int val;
+    public List<Nodes> children;
+
+    public Nodes() {}
+
+    public Nodes(int _val,List<Nodes> _children) {
+        val = _val;
+        children = _children;
+    }
+}
