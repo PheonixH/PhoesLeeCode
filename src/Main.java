b544329a42e9183db6b37f99df6d02e8e07ce25f
PheonixH <15927328287@163.com>
2020-01-08 16:02:27 +0800

    1291

--- src/Main.java
+++ src/Main.java
@@ -57,7 +57,7 @@ public class Main {
         return;
     }
 
-    public static void main(String[] args) {
+    public void test3(){
         Scanner in = new Scanner(System.in);
         while (in.hasNextInt()) {// 注意，如果输入是多个测试用例，请通过while循环处理多个测试用例
             int fr = in.nextInt();
@@ -91,6 +91,11 @@ public class Main {
             }
             System.out.println(res);
         }
+    }
+
+    public static void main(String[] args) {
+
+
         return;
     }
 }
