643fcb10087ed80e0f7260fdfc7f485e73a67ad4
huangfeng <19802021118@139.com>
2020-06-23 10:35:46 +0800

    solution 67

--- src/SolutionVmware.java
+++ src/SolutionVmware.java
@@ -938,6 +938,36 @@ public class SolutionVmware {
         }
     }
 
+
+    /**
+     * 67. 二进制求和
+     * 直接二进制求和
+     * */
+    public String addBinary0(String a, String b) {
+        return Integer.toBinaryString(
+                Integer.parseInt(a, 2) + Integer.parseInt(b, 2)
+        );
+    }
+
+
+    /**
+     * 67. 二进制求和
+     * 直接二进制求和
+     * */
+    public String addBinary(String a, String b) {
+        StringBuilder ans = new StringBuilder();
+        int ca = 0;
+        for(int i = a.length() - 1, j = b.length() - 1;i >= 0 || j >= 0; i--, j--) {
+            int sum = ca;
+            sum += i >= 0 ? a.charAt(i) - '0' : 0;
+            sum += j >= 0 ? b.charAt(j) - '0' : 0;
+            ans.append(sum % 2);
+            ca = sum / 2;
+        }
+        ans.append(ca == 1 ? ca : "");
+        return ans.reverse().toString();
+    }
+
     public static void main(String[] args) {
         SolutionVmware solutionVmware = new SolutionVmware();
         String[] strings = {"flower", "flow", "flight"};
@@ -954,7 +984,7 @@ public class SolutionVmware {
         t2.right = t4;
         int[][] goAhead = new int[][]{{1, 2, 0}, {4, 5, 1}};
         char[] chars = {'d', 'c', 'e', 'a', 'f', 'g', 'b'};
-        solutionVmware.setZeroes0(goAhead);
+        solutionVmware.addBinary("1000101", "101101");
 //        System.out.println(b);
         for (char c : chars) {
             System.out.println(c);
