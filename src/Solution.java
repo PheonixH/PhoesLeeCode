7c32543146865fb423b10d5e350c234a7dbd9982
huangfeng <vip443438>
2020-07-21 10:05:41 +0800

    95. 不同的二叉搜索树 II

--- src/Solution.java
+++ src/Solution.java
@@ -7223,6 +7223,119 @@ public class Solution {
         return res.toString();
     }
 
+    /**
+     * 95. 不同的二叉搜索树 II
+     * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：39.7 MB, 在所有 Java 提交中击败了14.29% 的用户
+     *
+     * @param n 一个整数 n
+     * @return 生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+     */
+    public List<TreeNode> generateTrees(int n) {
+        List<TreeNode> pre = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return pre;
+        }
+        pre.add(null);
+        //每次增加一个数字
+        for (int i = 1; i <= n; i++) {
+            List<TreeNode> cur = new ArrayList<TreeNode>();
+            //遍历之前的所有解
+            for (TreeNode root : pre) {
+                //插入到根节点
+                TreeNode insert = new TreeNode(i);
+                insert.left = root;
+                cur.add(insert);
+                //插入到右孩子，右孩子的右孩子...最多找 n 次孩子
+                for (int j = 0; j <= n; j++) {
+                    TreeNode root_copy = treeCopy(root); //复制当前的树
+                    TreeNode right = root_copy; //找到要插入右孩子的位置
+                    int k = 0;
+                    //遍历 j 次找右孩子
+                    for (; k < j; k++) {
+                        if (right == null) {
+                            break;
+                        }
+                        right = right.right;
+                    }
+                    //到达 null 提前结束
+                    if (right == null) {
+                        break;
+                    }
+                    //保存当前右孩子的位置的子树作为插入节点的左孩子
+                    TreeNode rightTree = right.right;
+                    insert = new TreeNode(i);
+                    right.right = insert; //右孩子是插入的节点
+                    insert.left = rightTree; //插入节点的左孩子更新为插入位置之前的子树
+                    //加入结果中
+                    cur.add(root_copy);
+                }
+            }
+            pre = cur;
+
+        }
+        return pre;
+    }
+
+
+    private TreeNode treeCopy(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode newRoot = new TreeNode(root.val);
+        newRoot.left = treeCopy(root.left);
+        newRoot.right = treeCopy(root.right);
+        return newRoot;
+    }
+
+    /**
+     * 95. 不同的二叉搜索树 II
+     * 给定一个整数 n，生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：40 MB, 在所有 Java 提交中击败了14.29% 的用户
+     *
+     * @param n 一个整数 n
+     * @return 生成所有由 1 ... n 为节点所组成的 二叉搜索树 。
+     */
+    public List<TreeNode> generateTrees1(int n) {
+        ArrayList<TreeNode>[] dp = new ArrayList[n + 1];
+        dp[0] = new ArrayList<TreeNode>();
+        if (n == 0) {
+            return dp[0];
+        }
+        dp[0].add(null);
+        //长度为 1 到 n
+        for (int len = 1; len <= n; len++) {
+            dp[len] = new ArrayList<TreeNode>();
+            //将不同的数字作为根节点，只需要考虑到 len
+            for (int root = 1; root <= len; root++) {
+                int left = root - 1;  //左子树的长度
+                int right = len - root; //右子树的长度
+                for (TreeNode leftTree : dp[left]) {
+                    for (TreeNode rightTree : dp[right]) {
+                        TreeNode treeRoot = new TreeNode(root);
+                        treeRoot.left = leftTree;
+                        //克隆右子树并且加上偏差
+                        treeRoot.right = clone(rightTree, root);
+                        dp[len].add(treeRoot);
+                    }
+                }
+            }
+        }
+        return dp[n];
+    }
+
+    private TreeNode clone(TreeNode n, int offset) {
+        if (n == null) {
+            return null;
+        }
+        TreeNode node = new TreeNode(n.val + offset);
+        node.left = clone(n.left, offset);
+        node.right = clone(n.right, offset);
+        return node;
+    }
+
     public static void main(String[] args) {
         Solution solution = new Solution();
 
