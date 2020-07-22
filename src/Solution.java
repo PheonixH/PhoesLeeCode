72b3d80f52056431ac7c4579be2f308571a46fa7
huangfeng <vip443438>
2020-07-22 14:47:19 +0800

    剑指 Offer 11. 旋转数组的最小数字

--- src/Solution.java
+++ src/Solution.java
@@ -7433,6 +7433,23 @@ public class Solution {
         return res.next;
     }
 
+    /**
+    * 剑指 Offer 11. 旋转数组的最小数字
+     * 把一个数组最开始的若干个元素搬到数组的末尾，我们称之为数组的旋转。输入一个递增排序的数组的一个旋转，输出旋转数组的最小元素。
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：39.9 MB, 在所有 Java 提交中击败了100.00% 的用户
+     * @param numbers 递增排序的数组
+     * @return
+     */
+    public int minArray(int[] numbers) {
+        int len = numbers.length;
+        for(int i = 1;i<len;i++){
+            if(numbers[i] < numbers[i-1]){
+                return numbers[i];
+            }
+        }
+        return numbers[0];
+    }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
