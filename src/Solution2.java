9df1405bcc06c13d60e2decc33d9d536b60f15a9
huangfeng <vip443438>
2020-07-24 11:43:16 +0800

    1104. 二叉树寻路

--- src/Solution2.java
+++ src/Solution2.java
@@ -1197,6 +1197,37 @@ public class Solution2 {
         return res;
     }
 
+    /**
+     * 1104. 二叉树寻路
+     * 在一棵无限的二叉树上，每个节点都有两个子节点，树中的节点 逐行 依次按 “之” 字形进行标记。
+     * 如下图所示，在奇数行（即，第一行、第三行、第五行……）中，按从左到右的顺序进行标记；
+     * 而偶数行（即，第二行、第四行、第六行……）中，按从右到左的顺序进行标记。
+     * 给你树上某一个节点的标号 label，请你返回从根节点到该标号为 label 节点的路径，该路径是由途经的节点标号所组成的。
+     *
+     * 执行用时：1 ms, 在所有 Java 提交中击败了51.00% 的用户
+     * 内存消耗：36.6 MB, 在所有 Java 提交中击败了33.33% 的用户
+     * @param l 点的标号 label
+     * @return  路径
+     */
+    public List<Integer> pathInZigZagTree(int l) {
+        if (l == 1) {
+            return new ArrayList<>() {
+                {
+                    add(1);
+                }
+            };
+        }
+        int i = 1;
+        while (l >= i) {
+            i = i << 1;
+        }
+        int pin = (l - (i >> 1)) / 2 + 1;
+        int resnow = (i >> 1) - pin;
+        List<Integer> res = pathInZigZagTree(resnow);
+        res.add(l);
+        return res;
+    }
+
 
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
