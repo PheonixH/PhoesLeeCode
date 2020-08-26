de108798eada5d1c412f1fbd9691f559fd46d16c
huangfeng <vip443438>
2020-08-26 15:41:50 +0800

    34. 在排序数组中查找元素的第一个和最后一个位置

--- src/Solution2.java
+++ src/Solution2.java
@@ -2412,6 +2412,55 @@ public class Solution2 {
         return dp[sLen - 1][pLen - 1] != 0;
     }
 
+
+    /**
+     * 34. 在排序数组中查找元素的第一个和最后一个位置
+     *
+     * 给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+     *
+     * 你的算法时间复杂度必须是 O(log n) 级别。
+     *
+     * 如果数组中不存在目标值，返回 [-1, -1]。
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：43.4 MB, 在所有 Java 提交中击败了5.15% 的用户
+     * @param nums 数组
+     * @param target 目标值
+     * @return 目标值在排序数组中查找元素的第一个和最后一个位置
+     */
+    public int[] searchRange(int[] nums, int target) {
+        int[] targetRange = {-1, -1};
+
+        int leftIdx = extremeInsertionIndex(nums, target, true);
+
+        // assert that `leftIdx` is within the array bounds and that `target`
+        // is actually in `nums`.
+        if (leftIdx == nums.length || nums[leftIdx] != target) {
+            return targetRange;
+        }
+
+        targetRange[0] = leftIdx;
+        targetRange[1] = extremeInsertionIndex(nums, target, false) - 1;
+
+        return targetRange;
+    }
+
+    private int extremeInsertionIndex(int[] nums, int target, boolean left) {
+        int lo = 0;
+        int hi = nums.length;
+
+        while (lo < hi) {
+            int mid = (lo + hi) / 2;
+            if (nums[mid] > target || (left && target == nums[mid])) {
+                hi = mid;
+            } else {
+                lo = mid + 1;
+            }
+        }
+
+        return lo;
+    }
+
+
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
 
