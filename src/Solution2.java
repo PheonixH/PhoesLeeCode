729cffcb2b40658b619130e41e203a5d687271bb
huangfeng <vip443438>
2020-08-31 09:43:46 +0800

    841. 钥匙和房间

--- src/Solution2.java
+++ src/Solution2.java
@@ -2819,6 +2819,46 @@ public class Solution2 {
         return ans;
     }
 
+    /**
+     * 841. 钥匙和房间
+     * <p>
+     * 有 N 个房间，开始时你位于 0 号房间。每个房间有不同的号码：0，1，2，...，N-1，并且房间里可能有一些钥匙能使你进入下一个房间。
+     * 在形式上，对于每个房间 i 都有一个钥匙列表 rooms[i]，每个钥匙 rooms[i][j] 由 [0,1，...，N-1] 中的一个整数表示，其中 N = rooms.length。 钥匙 rooms[i][j] = v 可以打开编号为 v 的房间。
+     * 最初，除 0 号房间外的其余所有房间都被锁住。
+     * 你可以自由地在房间之间来回走动。
+     * 如果能进入每个房间返回 true，否则返回 false。
+     * <p>
+     * 执行用时：3 ms, 在所有 Java 提交中击败了39.43% 的用户
+     * 内存消耗：39.7 MB, 在所有 Java 提交中击败了62.40% 的用户
+     *
+     * @param rooms 对应房间中的钥匙
+     * @return 是否全访问
+     */
+    public boolean canVisitAllRooms(List<List<Integer>> rooms) {
+        int len = rooms.size();
+        boolean[] visited = new boolean[len];
+        Stack<Integer> stack = new Stack<>();
+        for (int i : rooms.get(0)) {
+            stack.push(i);
+        }
+        int nums = 1;
+        visited[0] = true;
+        while (!stack.empty()) {
+            int tmp = stack.pop();
+            if (visited[tmp]) {
+                continue;
+            }
+            visited[tmp] = true;
+            for (int r : rooms.get(tmp)) {
+                stack.push(r);
+            }
+            nums++;
+            if (nums == len) {
+                break;
+            }
+        }
+        return nums == len;
+    }
 
     public static void main(String[] args) {
         Solution2 solution = new Solution2();
