aaf59389a3388193ac88ecc42cb55c4f36a54790
huangfeng <vip443438>
2020-09-11 16:26:39 +0800

    1376. 通知所有员工所需的时间

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -308,7 +308,7 @@ public class SolutionNow {
      * 内存消耗：49.7 MB, 在所有 Java 提交中击败了69.50% 的用户
      *
      * @param nums 数组 nums
-     * @param k 整数 k
+     * @param k    整数 k
      * @return 是否所有 1 都至少相隔 k 个元素
      */
     public boolean kLengthApart(int[] nums, int k) {
@@ -327,5 +327,38 @@ public class SolutionNow {
             }
         }
         return true;
+    }
+
+    /**
+     * 1376. 通知所有员工所需的时间
+     * 公司里有 n 名员工，每个员工的 ID 都是独一无二的，编号从 0 到 n - 1。公司的总负责人通过 headID 进行标识。
+     * 在 manager 数组中，每个员工都有一个直属负责人，其中 manager[i] 是第 i 名员工的直属负责人。对于总负责人，manager[headID] = -1。题目保证从属关系可以用树结构显示。
+     * 公司总负责人想要向公司所有员工通告一条紧急消息。他将会首先通知他的直属下属们，然后由这些下属通知他们的下属，直到所有的员工都得知这条紧急消息。
+     * 第 i 名员工需要 informTime[i] 分钟来通知它的所有直属下属（也就是说在 informTime[i] 分钟后，他的所有直属下属都可以开始传播这一消息）。
+     * 返回通知所有员工这一紧急消息所需要的 分钟数 。
+     *
+     * 执行用时：288 ms, 在所有 Java 提交中击败了14.48% 的用户
+     * 内存消耗：53.3 MB, 在所有 Java 提交中击败了72.65% 的用户
+     * @param n n名员工
+     * @param headID 总负责人
+     * @param manager 数组
+     * @param informTime 数组
+     * @return 通知所有员工所需的时间
+     */
+    public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
+        int tmp = 0;
+        int max = 0;
+        for (int i = 0; i < n; i++) {
+            if (informTime[i] == 0) {
+                int t = i;
+                while (t != headID) {
+                    t = manager[t];
+                    tmp += informTime[t];
+                }
+                max = Math.max(max, tmp);
+                tmp = 0;
+            }
+        }
+        return max;
     }
 }
