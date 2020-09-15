a7cfed9c75dda7def47480e1674076453979d7ea
huangfeng <vip443438>
2020-09-15 15:09:18 +0800

    面试题 02.07. 链表相交

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -638,4 +638,44 @@ public class SolutionNow {
         return true;
     }
 
+    /**
+     * 面试题 02.07. 链表相交
+     * 给定两个（单向）链表，判定它们是否相交并返回交点。请注意相交的定义基于节点的引用，而不是基于节点的值。换句话说，如果一个链表的第k个节点与另一个链表的第j个节点是同一节点（引用完全相同），则这两个链表相交。
+     * <p>
+     * 执行用时：1 ms, 在所有 Java 提交中击败了99.94% 的用户
+     * 内存消耗：42.5 MB, 在所有 Java 提交中击败了73.19% 的用户
+     *
+     * @param headA 链表A
+     * @param headB 链表B
+     * @return 相交链表
+     */
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        int lenA = 0, lenB = 0;
+        ListNode ha = headA;
+        while (ha != null) {
+            ha = ha.next;
+            lenA++;
+        }
+        ListNode hb = headB;
+        while (hb != null) {
+            hb = hb.next;
+            lenB++;
+        }
+        while (lenA > lenB) {
+            headA = headA.next;
+            lenA--;
+        }
+        while (lenA < lenB) {
+            headB = headB.next;
+            lenB--;
+        }
+        while (headA != null) {
+            if (headA.equals(headB)) {
+                return headA;
+            }
+            headA = headA.next;
+            headB = headB.next;
+        }
+        return null;
+    }
 }
