89e50ddb535b0b136446cd65ade019b76bbd7ac2
huangfeng <vip443438>
2020-09-16 09:24:57 +0800

    226. 翻转二叉树

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -738,4 +738,26 @@ public class SolutionNow {
         }
         return Math.max(l, r);
     }
+
+    /**
+     * 226. 翻转二叉树
+     * 翻转一棵二叉树。
+     * <p>
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：36.2 MB, 在所有 Java 提交中击败了90.36% 的用户
+     *
+     * @param root 二叉树
+     * @return 翻转二叉树
+     */
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return root;
+        }
+        TreeNode tmp = root.left;
+        root.left = root.right;
+        root.right = tmp;
+        invertTree(root.left);
+        invertTree(root.right);
+        return root;
+    }
 }
