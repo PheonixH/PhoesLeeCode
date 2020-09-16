9d436f03cc59da324b312f46b6e4155df0fbccba
huangfeng <vip443438>
2020-09-16 10:19:39 +0800

    Main TreeNode create change

--- src/leetcode/Main.java
+++ src/leetcode/Main.java
@@ -18,10 +18,8 @@ import java.util.stream.Collectors;
  */
 public class Main {
 
-    public static void main(String[] args) {
-
+    public static ListNode createListNode(int[] listNodeValue) {
         //ListNode
-        int[] listNodeValue = {1, 2, 3, 3, 2, 1};
         int listNodeLen = listNodeValue.length;
         ListNode head = new ListNode(listNodeValue[0]);
         ListNode listNodeTmp = head;
@@ -29,10 +27,12 @@ public class Main {
             listNodeTmp.next = new ListNode(listNodeValue[i]);
             listNodeTmp = listNodeTmp.next;
         }
+        return head;
+    }
 
+    public static TreeNode createTreeNode(int[] treeNodeValue) {
         //TreeNode
         //-1 is null
-        int[] treeNodeValue = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
         List<TreeNode> createTreeNodeStack = new ArrayList<>();
         TreeNode root = new TreeNode(treeNodeValue[0]);
         createTreeNodeStack.add(root);
@@ -56,14 +56,35 @@ public class Main {
             }
             createTreeNodeStack.add(tmp.right);
         }
+        return root;
+    }
 
+    public static List<List<Integer>> createListListInteger(int[][] listListIntegerArray) {
+        List<List<Integer>> integerListList = new LinkedList<>();
+        for (int[] listIntegerArray : listListIntegerArray) {
+            List<Integer> collect = Arrays.stream(listIntegerArray).boxed().collect(Collectors.toList());
+            integerListList.add(collect);
+        }
+        return integerListList;
+    }
+
+    public static <T> List<List<T>> createListList(T[][] listListArray) {
+        List<List<T>> stringListList = new LinkedList<>();
+        for (T[] listStringArray : listListArray) {
+            List<T> collect = Arrays.stream(listStringArray).collect(Collectors.toList());
+            stringListList.add(collect);
+        }
+        return stringListList;
+    }
+
+
+    public static void main(String[] args) {
 
         //Arrays
         String[] oneDimensionalStringArray = {"eat", "tea", "tan", "ate", "nat", "bat"};
         int[] oneDimensionalArrayA = {1, 0, 0, 0, 1, 0, 0, 1};
         int[] oneDimensionalArrayB = {5, 2, 2, 5, 3, 5};
-        int[][] twoDimensionalArrayA = {{0, 1}};
-        int[][] twoDimensionalArrayB = {
+        int[][] twoDimensionalArray = {
                 {0, 8, 7, 10, 9, 10, 0, 9, 6},
                 {8, 7, 10, 8, 7, 4, 9, 6, 10},
                 {8, 1, 1, 5, 1, 5, 5, 1, 2},
@@ -86,20 +107,17 @@ public class Main {
 
         //List<List<Integer>>
         int[][] listListIntegerArray = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
-        List<List<Integer>> integerListList = new LinkedList<>();
-        for (int[] listIntegerArray : listListIntegerArray) {
-            List<Integer> collect = Arrays.stream(listIntegerArray).boxed().collect(Collectors.toList());
-            integerListList.add(collect);
-        }
+        List<List<Integer>> intListList = createListListInteger(listListIntegerArray);
 
-        //List<List<String>>
+        //List<List<String>> 非Integer
         String[][] listListStringArray = {{"JFK", "SFO"}, {"JFK", "ATL"}, {"SFO", "ATL"}, {"ATL", "JFK"}, {"ATL", "SFO"}};
-        List<List<String>> stringListList = new LinkedList<>();
-        for (String[] listStringArray : listListStringArray) {
-            List<String> collect = Arrays.stream(listStringArray).collect(Collectors.toList());
-            stringListList.add(collect);
-        }
+        List<List<String>> stringListList = createListList(listListStringArray);
 
+        int[] listNodeValue = {1, 2, 3, 3, 2, 1};
+        ListNode head = createListNode(listNodeValue);
+
+        int[] treeNodeValue = {6, 2, 8, 0, 4, 7, 9, -1, -1, 3, 5};
+        TreeNode root = createTreeNode(treeNodeValue);
 
         SolutionNow solution = new SolutionNow();
         System.out.println(solution.inorderSuccessor(root, root.left));
