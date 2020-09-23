52e2860f42f52001e21046f3334bc1878941dd19
huangfeng <vip443438>
2020-09-23 11:39:37 +0800

    645. 错误的集合

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1246,4 +1246,35 @@ public class SolutionNow {
         }
         return t1;
     }
+
+    /**
+     * 645. 错误的集合
+     * 集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重复。
+     * 给定一个数组 nums 代表了集合 S 发生错误后的结果。你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。
+     * 执行用时：2 ms, 在所有 Java 提交中击败了96.23% 的用户
+     * 内存消耗：39.9 MB, 在所有 Java 提交中击败了94.79% 的用户
+     * <p>
+     * 用异或寻找更佳哦
+     *
+     * @param nums 数组
+     * @return 错误的集合
+     */
+    public int[] findErrorNums(int[] nums) {
+        boolean[] booleans = new boolean[nums.length];
+        int[] res = new int[2];
+        for (int n : nums) {
+            if (!booleans[n - 1]) {
+                booleans[n - 1] = true;
+            } else {
+                res[0] = n;
+            }
+        }
+        for (int i = 0; i < nums.length; i++) {
+            if (!booleans[i]) {
+                res[1] = i + 1;
+                break;
+            }
+        }
+        return res;
+    }
 }
