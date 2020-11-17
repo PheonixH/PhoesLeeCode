3379292716cf48d180a8df50c832599b6250abb8
huangfeng <vip443438>
2020-11-17 09:30:38 +0800

    1030. 距离顺序排列矩阵单元格

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1846,6 +1846,39 @@ public class SolutionNow {
 
         return res;
     }
+
+    /**
+     * 1030. 距离顺序排列矩阵单元格
+     * 给出 R 行 C 列的矩阵，其中的单元格的整数坐标为 (r, c)，满足 0 <= r < R 且 0 <= c < C。
+     * 另外，我们在该矩阵中给出了一个坐标为 (r0, c0) 的单元格。
+     * 返回矩阵中的所有单元格的坐标，并按到 (r0, c0) 的距离从最小到最大的顺序排，
+     * 其中，两单元格(r1, c1) 和 (r2, c2) 之间的距离是曼哈顿距离，|r1 - r2| + |c1 - c2|。（你可以按任何满足此条件的顺序返回答案。）
+     * <p>
+     * 执行用时：14 ms, 在所有 Java 提交中击败了73.79% 的用户
+     * 内存消耗：41.1 MB, 在所有 Java 提交中击败了18.54% 的用户
+     *
+     * @param R R行
+     * @param C C列
+     * @param r0 坐标为 (r0, c0) 的单元格
+     * @param c0 坐标为 (r0, c0) 的单元格
+     * @return 返回矩阵中的所有单元格的坐标，并按到 (r0, c0) 的距离从最小到最大的顺序排
+     */
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] ret = new int[R * C][];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                ret[i * C + j] = new int[]{i, j};
+            }
+        }
+        Arrays.sort(ret, new Comparator<int[]>() {
+            @Override
+            public int compare(int[] a, int[] b) {
+                return (Math.abs(a[0] - r0) + Math.abs(a[1] - c0)) - (Math.abs(b[0] - r0) + Math.abs(b[1] - c0));
+            }
+        });
+        return ret;
+
+    }
 }
 //class Node {
 //    public int val;
