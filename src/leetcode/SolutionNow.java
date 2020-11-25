0f9826c1dced59a3ac5a4b79ff8a11bde6df3c4c
huangfeng <vip443438>
2020-11-25 09:35:49 +0800

    1370. 上升下降字符串

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -2277,6 +2277,70 @@ public class SolutionNow {
         }
         return ans;
     }
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        int n = nums.length;
+        int[] ans = new int[n - k + 1];
+        Arrays.fill(ans, Integer.MIN_VALUE);
+        for (int i = 0; i < k; i++) {
+            ans[0] = Math.max(ans[0], nums[i]);
+        }
+        for (int i = 1; i < n - k; i++) {
+            if (ans[i - 1] == nums[i - 1]) {
+                for (int j = i; j < k + i; j++) {
+                    ans[i] = Math.max(ans[i], nums[j]);
+                }
+            } else {
+                ans[i - k] = Math.max(ans[i - 1 - k], nums[i]);
+            }
+        }
+        return ans;
+    }
+
+
+    /**
+     * 1370. 上升下降字符串
+     * <p>
+     * 给你一个字符串 s ，请你根据下面的算法重新构造字符串：
+     * 从 s 中选出 最小 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最小 的字符，且该字符比上一个添加的字符大，将它 接在 结果字符串后面。
+     * 重复步骤 2 ，直到你没法从 s 中选择字符。
+     * 从 s 中选出 最大 的字符，将它 接在 结果字符串的后面。
+     * 从 s 剩余字符中选出 最大 的字符，且该字符比上一个添加的字符小，将它 接在 结果字符串后面。
+     * 重复步骤 5 ，直到你没法从 s 中选择字符。
+     * 重复步骤 1 到 6 ，直到 s 中所有字符都已经被选过。
+     * 在任何一步中，如果最小或者最大字符不止一个 ，你可以选择其中任意一个，并将其添加到结果字符串。
+     * 请你返回将 s 中字符重新排序后的 结果字符串 。
+     * <p>
+     * 执行用时：3 ms, 在所有 Java 提交中击败了98.12% 的用户
+     * 内存消耗：38.7 MB, 在所有 Java 提交中击败了67.31% 的用户
+     *
+     * @param s 字符串
+     * @return 上升下降字符串
+     */
+    public String sortString(String s) {
+        int[] nums = new int[26];
+        int n = s.length();
+        for (int i = 0; i < n; i++) {
+            nums[s.charAt(i) - 'a']++;
+        }
+        StringBuilder stringBuilder = new StringBuilder();
+        while (stringBuilder.length() < n) {
+            for (int i = 0; i < 26; i++) {
+                if (nums[i] > 0) {
+                    stringBuilder.append((char) ('a' + i));
+                    nums[i]--;
+                }
+            }
+            for (int i = 25; i >= 0; i--) {
+                if (nums[i] > 0) {
+                    stringBuilder.append((char) ('a' + i));
+                    nums[i]--;
+                }
+            }
+        }
+        return stringBuilder.toString();
+    }
 }
 
 
