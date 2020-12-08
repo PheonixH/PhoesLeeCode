275f76acd5a33972ef376d93ae11fe5f906f2ee2
huangfeng <vip443438>
2020-12-08 17:06:38 +0800

    1047. 删除字符串中的所有相邻重复项

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -117,4 +117,26 @@ public class SolutionNow {
         }
         return sum == 0 ? null : head;
     }
+
+    /**
+     * 1047. 删除字符串中的所有相邻重复项
+     * <p>
+     * 给出由小写字母组成的字符串 S，重复项删除操作会选择两个相邻且相同的字母，并删除它们。
+     * 在 S 上反复执行重复项删除操作，直到无法继续删除。
+     * 在完成所有重复项删除操作后返回最终的字符串。答案保证唯一。
+     * <p>
+     * 执行用时：52 ms, 在所有 Java 提交中击败了19.67% 的用户
+     * 内存消耗：39 MB, 在所有 Java 提交中击败了78.77% 的用户
+     *
+     * @param S 小写字母组成的字符串
+     * @return 在完成所有重复项删除操作后返回最终的字符串
+     */
+    public String removeDuplicates(String S) {
+        String newS = S;
+        for (char c = 'a'; c <= 'z'; c++) {
+            String remove = "" + c + c;
+            newS = newS.replace(remove, "");
+        }
+        return newS.equals(S) ? S : removeDuplicates(newS);
+    }
 }
