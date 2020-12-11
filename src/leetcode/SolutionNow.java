0b44ccc0fb896ec80a146bbfdd8bde7343e6ad86
huangfeng <vip443438>
2020-12-11 14:44:37 +0800

    649. Dota2 参议院

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -247,10 +247,11 @@ public class SolutionNow {
     /**
      * 1344. 时钟指针的夹角
      * 给你两个数 hour 和 minutes 。请你返回在时钟上，由给定时间的时针和分针组成的较小角的角度（60 单位制）。
-     *
+     * <p>
      * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
      * 内存消耗：35.6 MB, 在所有 Java 提交中击败了49.09% 的用户
-     * @param hour 数
+     *
+     * @param hour    数
      * @param minutes 数
      * @return 时钟指针的夹角
      */
@@ -259,5 +260,60 @@ public class SolutionNow {
         double h = (double) hour % 12 * 30 + (double) minutes / 2;
         double r = Math.abs(m - h);
         return Math.min(r, 360 - r);
+    }
+
+    /**
+     * 649. Dota2 参议院
+     * Dota2 的世界里有两个阵营：Radiant(天辉)和 Dire(夜魇)
+     * Dota2 参议院由来自两派的参议员组成。现在参议院希望对一个 Dota2 游戏里的改变作出决定。他们以一个基于轮为过程的投票进行。在每一轮中，每一位参议员都可以行使两项权利中的一项：
+     * 禁止一名参议员的权利：
+     * 参议员可以让另一位参议员在这一轮和随后的几轮中丧失所有的权利。
+     * 宣布胜利：
+     * 如果参议员发现有权利投票的参议员都是同一个阵营的，他可以宣布胜利并决定在游戏中的有关变化。
+     * 给定一个字符串代表每个参议员的阵营。字母 “R” 和 “D” 分别代表了 Radiant（天辉）和 Dire（夜魇）。然后，如果有 n 个参议员，给定字符串的大小将是 n。
+     * 以轮为基础的过程从给定顺序的第一个参议员开始到最后一个参议员结束。这一过程将持续到投票结束。所有失去权利的参议员将在过程中被跳过。
+     * 假设每一位参议员都足够聪明，会为自己的政党做出最好的策略，你需要预测哪一方最终会宣布胜利并在 Dota2 游戏中决定改变。输出应该是 Radiant 或 Dire。
+     * 执行用时：2 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：38.7 MB, 在所有 Java 提交中击败了73.05% 的用户
+     *
+     * @param senate
+     * @return
+     */
+    public String predictPartyVictory(String senate) {
+        return predictPartyVictory(senate, 0, 0);
+    }
+
+    public String predictPartyVictory(String senate, int radiant, int dire) {
+        int n = senate.length();
+        StringBuilder stringBuilder = new StringBuilder();
+        boolean radiantWin = true;
+        boolean direWin = true;
+        for (int i = 0; i < n; i++) {
+            char c = senate.charAt(i);
+            if (c == 'R') {
+                if (dire > 0) {
+                    dire--;
+                } else {
+                    radiant++;
+                    stringBuilder.append(c);
+                    direWin = false;
+                }
+            } else {
+                if (radiant > 0) {
+                    radiant--;
+                } else {
+                    dire++;
+                    stringBuilder.append(c);
+                    radiantWin = false;
+                }
+            }
+        }
+        if (radiantWin) {
+            return "Radiant";
+        } else if (direWin) {
+            return "Dire";
+        } else {
+            return predictPartyVictory(stringBuilder.toString(), radiant, dire);
+        }
     }
 }
