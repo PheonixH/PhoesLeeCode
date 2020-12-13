1ef20e08f32c43ea116202882a80af105b27d600
15927328287@163.com <36834526+PheonixH@users.noreply.github.com>
2020-12-13 10:19:50 +0800

    217. 存在重复元素

--- src/leetcode/SolitionUbuntu.java
+++ src/leetcode/SolitionUbuntu.java
@@ -2686,4 +2686,25 @@ x = (a[7]b[7]) (a[6]b[6]) ... (a[1]b[1]) (a[0]b[0])
         }
         return father[p] = find(father, father[p]);
     }
+
+    /**
+     * 217. 存在重复元素
+     *
+     * 给定一个整数数组，判断是否存在重复元素。
+     * 如果任意一值在数组中出现至少两次，函数返回 true 。如果数组中每个元素都不相同，则返回 false 。
+     *
+     * 执行用时：5 ms, 在所有 Java 提交中击败了76.63% 的用户
+     * 内存消耗：42.5 MB, 在所有 Java 提交中击败了76.75% 的用户
+     * @param nums 数组
+     * @return 是否存在重复的元素
+     */
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set = new HashSet<Integer>();
+        for (int x : nums) {
+            if (!set.add(x)) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
