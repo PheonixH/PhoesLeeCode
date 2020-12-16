a13fa99002404b7b306cbf6e4cd323e0da829c4c
huangfeng <vip443438>
2020-12-16 10:48:24 +0800

    1406. 石子游戏 III

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -596,4 +596,46 @@ public class SolutionNow {
         }
         return ans;
     }
+
+    /**
+     * 1406. 石子游戏 III
+     * <p>
+     * Alice 和 Bob 用几堆石子在做游戏。几堆石子排成一行，每堆石子都对应一个得分，由数组 stoneValue 给出。
+     * Alice 和 Bob 轮流取石子，Alice 总是先开始。在每个玩家的回合中，该玩家可以拿走剩下石子中的的前 1、2 或 3 堆石子 。
+     * 比赛一直持续到所有石头都被拿走。每个玩家的最终得分为他所拿到的每堆石子的对应得分之和。
+     * 每个玩家的初始分数都是 0 。比赛的目标是决出最高分，得分最高的选手将会赢得比赛，比赛也可能会出现平局。
+     * 假设 Alice 和 Bob 都采取 最优策略 。如果 Alice 赢了就返回 "Alice" ，Bob 赢了就返回 "Bob"，平局（分数相同）返回 "Tie" 。
+     * <p>
+     * <p>
+     * 执行用时：9 ms, 在所有 Java 提交中击败了91.46% 的用户
+     * 内存消耗：48 MB, 在所有 Java 提交中击败了61.93% 的用户
+     *
+     * @param stoneValue 几堆石子排成一行
+     * @return 获胜者
+     */
+    public String stoneGameIII(int[] stoneValue) {
+        int n = stoneValue.length;
+        int[] suffixSum = new int[n];
+        suffixSum[n - 1] = stoneValue[n - 1];
+        for (int i = n - 2; i >= 0; --i) {
+            suffixSum[i] = suffixSum[i + 1] + stoneValue[i];
+        }
+        int[] f = new int[n + 1];
+        // 边界情况，当没有石子时，分数为 0
+        // 为了代码的可读性，显式声明
+        f[n] = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            int bestj = f[i + 1];
+            for (int j = i + 2; j <= i + 3 && j <= n; j++) {
+                bestj = Math.min(bestj, f[j]);
+            }
+            f[i] = suffixSum[i] - bestj;
+        }
+        int total = suffixSum[0];
+        if (f[0] * 2 == total) {
+            return "Tie";
+        } else {
+            return f[0] * 2 > total ? "Alice" : "Bob";
+        }
+    }
 }
