a8e590dfce872270d6857a04cf1b66989514b347
huangfeng <vip443438>
2020-12-25 11:30:24 +0800

    LCP 17. 速算机器人

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1386,4 +1386,29 @@ public class SolutionNow {
         }
         return ans;
     }
+
+    /**
+     * LCP 17. 速算机器人
+     *
+     * 小扣在秋日市集发现了一款速算机器人。店家对机器人说出两个数字（记作 x 和 y），请小扣说出计算指令：
+     *     "A" 运算：使 x = 2 * x + y；
+     *     "B" 运算：使 y = 2 * y + x。
+     * 在本次游戏中，店家说出的数字为 x = 1 和 y = 0，小扣说出的计算指令记作仅由大写字母 A、B 组成的字符串 s，
+     * 字符串中字符的顺序表示计算顺序，请返回最终 x 与 y 的和为多少。
+     *
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：36 MB, 在所有 Java 提交中击败了92.15% 的用户
+     * @param s 字符串
+     * @return 最终 x 与 y 的和为多少
+     */
+    public int calculate(String s) {
+        int x = 1, y = 0;
+        for (char c : s.toCharArray()) {
+            switch (c) {
+                case 'A' -> x = 2 * x + y;
+                case 'B' -> y = 2 * y + x;
+            }
+        }
+        return x + y;
+    }
 }
