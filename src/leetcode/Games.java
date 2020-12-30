8199c2e95760e2f8b75c364c85e4f5c06490c01d
15927328287@163.com <36834526+PheonixH@users.noreply.github.com>
2020-12-30 09:41:35 +0800

    1046. 最后一块石头的重量

--- src/leetcode/Games.java
+++ src/leetcode/Games.java
@@ -1643,4 +1643,35 @@ public class Games {
         return ans;
     }
 
+    /**
+     * 1046. 最后一块石头的重量
+     *
+     * 有一堆石头，每块石头的重量都是正整数。
+     * 每一回合，从中选出两块 最重的 石头，然后将它们一起粉碎。假设石头的重量分别为 x 和 y，且 x <= y。那么粉碎的可能结果如下：
+     *     如果 x == y，那么两块石头都会被完全粉碎；
+     *     如果 x != y，那么重量为 x 的石头将会完全粉碎，而重量为 y 的石头新重量为 y-x。
+     * 最后，最多只会剩下一块石头。返回此石头的重量。如果没有石头剩下，就返回 0。
+     *
+     * 执行用时：1 ms, 在所有 Java 提交中击败了80.11% 的用户
+     * 内存消耗：35.6 MB, 在所有 Java 提交中击败了87.37% 的用户
+     * @param stones 每块石头的重量
+     * @return 最后一块石头的重量
+     */
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> priorityQueue = new PriorityQueue<Integer>((a, b) -> b - a);
+        Arrays.stream(stones).forEach(priorityQueue::add);
+        int pre = priorityQueue.poll();
+        while (!priorityQueue.isEmpty()) {
+            int tmp = priorityQueue.poll();
+            if (pre > tmp) {
+                priorityQueue.add(pre - tmp);
+            }
+            if (priorityQueue.isEmpty()) {
+                pre = 0;
+            } else {
+                pre = priorityQueue.poll();
+            }
+        }
+        return pre;
+    }
 }
