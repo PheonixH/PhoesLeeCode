d0e8b5ed69c593088b43bd700566a86be1524116
huangfeng <vip443438>
2021-01-15 10:16:51 +0800

    947. 移除最多的同行或同列石头

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import Template.BinaryIndexedTree;
+import Template.UnionFind;
 import leetcode.dataStruct.ListNode;
 import leetcode.dataStruct.TreeNode;
 
@@ -19,4 +20,35 @@ import java.util.stream.Collectors;
  */
 public class SolutionNow {
 
+    /**
+     * 947. 移除最多的同行或同列石头
+     *
+     * n 块石头放置在二维平面中的一些整数坐标点上。每个坐标点上最多只能有一块石头。
+     * 如果一块石头的 同行或者同列 上有其他石头存在，那么就可以移除这块石头。
+     * 给你一个长度为 n 的数组 stones ，其中 stones[i] = [xi, yi] 表示第 i 块石头的位置，
+     * 返回 可以移除的石子 的最大数量。
+     *
+     * 执行用时：39 ms, 在所有 Java 提交中击败了29.85% 的用户
+     * 内存消耗：39 MB, 在所有 Java 提交中击败了22.84% 的用户
+     * @param stones 石头所在整数坐标点
+     * @return 可以移除的石子 的最大数量
+     */
+    public int removeStones(int[][] stones) {
+        int n = stones.length;
+        UnionFind unionFind = new UnionFind(n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) {
+                    unionFind.union(i, j);
+                }
+            }
+        }
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (unionFind.root(i) == i) {
+                ans++;
+            }
+        }
+        return n - ans;
+    }
 }
