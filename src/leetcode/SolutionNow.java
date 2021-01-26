0fc5b42d60517d5b41fecd5db41f4893c8a2c51b
huangfeng <vip443438>
2021-01-26 09:44:17 +0800

    1128. 等价多米诺骨牌对的数量

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -22,14 +22,15 @@ public class SolutionNow {
 
     /**
      * 947. 移除最多的同行或同列石头
-     *
+     * <p>
      * n 块石头放置在二维平面中的一些整数坐标点上。每个坐标点上最多只能有一块石头。
      * 如果一块石头的 同行或者同列 上有其他石头存在，那么就可以移除这块石头。
      * 给你一个长度为 n 的数组 stones ，其中 stones[i] = [xi, yi] 表示第 i 块石头的位置，
      * 返回 可以移除的石子 的最大数量。
-     *
+     * <p>
      * 执行用时：39 ms, 在所有 Java 提交中击败了29.85% 的用户
      * 内存消耗：39 MB, 在所有 Java 提交中击败了22.84% 的用户
+     *
      * @param stones 石头所在整数坐标点
      * @return 可以移除的石子 的最大数量
      */
@@ -50,5 +51,32 @@ public class SolutionNow {
             }
         }
         return n - ans;
+    }
+
+    /**
+     * 1128. 等价多米诺骨牌对的数量
+     * <p>
+     * 给你一个由一些多米诺骨牌组成的列表 dominoes。
+     * 如果其中某一张多米诺骨牌可以通过旋转 0 度或 180 度得到另一张多米诺骨牌，我们就认为这两张牌是等价的。
+     * 形式上，dominoes[i] = [a, b] 和 dominoes[j] = [c, d] 等价的前提是 a==c 且 b==d，或是 a==d 且 b==c。
+     * 在 0 <= i < j < dominoes.length 的前提下，找出满足 dominoes[i] 和 dominoes[j] 等价的骨牌对 (i, j) 的数量。
+     * <p>
+     * 执行用时：31 ms, 在所有 Java 提交中击败了8.78% 的用户
+     * 内存消耗：48 MB, 在所有 Java 提交中击败了8.64% 的用户
+     *
+     * @param dominoes 多米诺骨牌
+     * @return 等价的骨牌对 (i, j) 的数量
+     */
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<String, Integer> map = new HashMap<>();
+        int ans = 0;
+        for (int[] domino : dominoes) {
+            Arrays.sort(domino);
+            String key = domino[0] + "," + domino[1];
+            int value = map.getOrDefault(key, 0) + 1;
+            map.put(key, value);
+            ans += value - 1;
+        }
+        return ans;
     }
 }
