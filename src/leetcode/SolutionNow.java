a81ff44a37847c3fce37abb4056ad4e017c5c88e
15927328287@163.com <36834526+PheonixH@users.noreply.github.com>
2021-02-01 22:09:00 +0800

    888. 公平的糖果棒交换

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -475,12 +475,13 @@ public class SolutionNow {
 
     /**
      * 面试题 16.19. 水域大小
-     *
+     * <p>
      * 你有一个用于表示一片土地的整数矩阵land，该矩阵中每个点的值代表对应地点的海拔高度。若值为0则表示水域。由垂直、水平或对角连接的水域为池塘。
      * 池塘的大小是指相连接的水域的个数。编写一个方法来计算矩阵中所有池塘的大小，返回值需要从小到大排序。
-     *
+     * <p>
      * 执行用时：15 ms, 在所有 Java 提交中击败了59.24% 的用户
      * 内存消耗：62.9 MB, 在所有 Java 提交中击败了62.57% 的用户
+     *
      * @param land 一片土地的整数矩阵land
      * @return 所有池塘的大小，返回值需要从小到大排序
      */
@@ -516,4 +517,45 @@ public class SolutionNow {
         return ans;
     }
 
+
+    /**
+     * 888. 公平的糖果棒交换
+     * <p>
+     * 爱丽丝和鲍勃有不同大小的糖果棒：A[i] 是爱丽丝拥有的第 i 根糖果棒的大小，B[j] 是鲍勃拥有的第 j 根糖果棒的大小。
+     * 因为他们是朋友，所以他们想交换一根糖果棒，这样交换后，他们都有相同的糖果总量。（一个人拥有的糖果总量是他们拥有的糖果棒大小的总和。）
+     * 返回一个整数数组 ans，其中 ans[0] 是爱丽丝必须交换的糖果棒的大小，ans[1] 是 Bob 必须交换的糖果棒的大小。
+     * 如果有多个答案，你可以返回其中任何一个。保证答案存在。
+     * <p>
+     * 执行用时：26 ms, 在所有 Java 提交中击败了28.10% 的用户
+     * 内存消耗：40 MB, 在所有 Java 提交中击败了66.32% 的用户
+     *
+     * @param A 糖果棒
+     * @param B 糖果棒
+     * @return 交换的糖果棒
+     */
+    public int[] fairCandySwap(int[] A, int[] B) {
+        Arrays.sort(A);
+        Arrays.sort(B);
+        int sumA = Arrays.stream(A).sum();
+        int sumB = Arrays.stream(B).sum();
+        int dir = (sumA - sumB) / 2;
+        for (int i : A) {
+            if (i - dir > 0) {
+                int target = i - dir;
+                int l = 0;
+                int r = B.length - 1;
+                while (l <= r) {
+                    int mid = (l + r) / 2;
+                    if (B[mid] < target) {
+                        l = mid + 1;
+                    } else if (B[mid] > target) {
+                        r = mid - 1;
+                    } else {
+                        return new int[]{i, target};
+                    }
+                }
+            }
+        }
+        return null;
+    }
 }
