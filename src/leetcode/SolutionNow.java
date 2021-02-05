97c982ea92497b5e04908aa4e117cb27ac8f64b8
huangfeng <19802021118@139.com>
2021-02-05 09:57:59 +0800

    520. 检测大写字母

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -726,4 +726,31 @@ public class SolutionNow {
         }
         return s.length() - left;
     }
+
+    /**
+     * 520. 检测大写字母
+     * <p>
+     * 给定一个单词，你需要判断单词的大写使用是否 正确。
+     * 我们定义，在以下情况时，单词的大写用法是正确的：
+     * 全部字母都是大写，比如"USA"。
+     * 单词中所有字母都不是大写，比如"leetcode"。
+     * 如果单词不只含有一个字母，只有首字母大写， 比如 "Google"。
+     * 否则，我们定义这个单词没有正确使用大写字母。
+     * <p>
+     * 执行用时：1 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：36.7 MB, 在所有 Java 提交中击败了86.32% 的用户
+     *
+     * @param word 单词
+     * @return 单词的大写使用是否 正确
+     */
+    public boolean detectCapitalUse(String word) {
+        int n = word.length();
+        int num = 0;
+        for (char c : word.toCharArray()) {
+            if (c <= 'Z') {
+                num++;
+            }
+        }
+        return num == 0 || num == n || (num == 1 && word.charAt(0) <= 'Z');
+    }
 }
