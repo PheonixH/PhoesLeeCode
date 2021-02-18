be0aa5aa39dc7c497e3b342c5200db88eb79daa4
huangfeng <19802021118@139.com>
2021-02-18 09:53:30 +0800

    995. K 连续位的最小翻转次数

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -868,4 +868,34 @@ public class SolutionNow {
         }
         return num == 0 || num == n || (num == 1 && word.charAt(0) <= 'Z');
     }
+
+    /**
+     * 995. K 连续位的最小翻转次数
+     *
+     * 在仅包含 0 和 1 的数组 A 中，一次 K 位翻转包括选择一个长度为 K 的（连续）子数组，同时将子数组中的每个 0 更改为 1，而每个 1 更改为 0。
+     * 返回所需的 K 位翻转的最小次数，以便数组没有值为 0 的元素。如果不可能，返回 -1。
+     *
+     * 执行用时：6 ms, 在所有 Java 提交中击败了83.23% 的用户
+     * 内存消耗：46.9 MB, 在所有 Java 提交中击败了16.25% 的用户
+     * @param A 数组
+     * @param K 整数
+     * @return 连续位的最小翻转次数
+     */
+    public int minKBitFlips(int[] A, int K) {
+        int n = A.length;
+        int[] diff = new int[n + 1];
+        int ans = 0, revCnt = 0;
+        for (int i = 0; i < n; ++i) {
+            revCnt += diff[i];
+            if ((A[i] + revCnt) % 2 == 0) {
+                if (i + K > n) {
+                    return -1;
+                }
+                ++ans;
+                ++revCnt;
+                --diff[i + K];
+            }
+        }
+        return ans;
+    }
 }
