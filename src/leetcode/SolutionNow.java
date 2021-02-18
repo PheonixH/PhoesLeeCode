0f3c28649800054725c5909e8ee8009d3c91e43b
huangfeng <19802021118@139.com>
2021-02-18 11:49:53 +0800

    998. 最大二叉树 II

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1,6 +1,7 @@
 package leetcode;
 
 import Template.UnionFind;
+import leetcode.dataStruct.TreeNode;
 
 import java.util.*;
 
@@ -950,5 +951,39 @@ public class SolutionNow {
 
         }
         return min;
+    }
+
+    /**
+     * 998. 最大二叉树 II
+     * 最大树定义：一个树，其中每个节点的值都大于其子树中的任何其他值。
+     * 给出最大树的根节点 root。
+     * 就像之前的问题那样，给定的树是从列表 A（root = Construct(A)）递归地使用下述 Construct(A) 例程构造的：
+     * 如果 A 为空，返回 null
+     * 否则，令 A[i] 作为 A 的最大元素。创建一个值为 A[i] 的根节点 root
+     * root 的左子树将被构建为 Construct([A[0], A[1], ..., A[i-1]])
+     * root 的右子树将被构建为 Construct([A[i+1], A[i+2], ..., A[A.length - 1]])
+     * 返回 root
+     * 请注意，我们没有直接给定 A，只有一个根节点 root = Construct(A).
+     * 假设 B 是 A 的副本，并在末尾附加值 val。题目数据保证 B 中的值是不同的。
+     * 返回 Construct(B)。
+     * <p>
+     * 执行用时：0 ms, 在所有 Java 提交中击败了100.00% 的用户
+     * 内存消耗：36.3 MB, 在所有 Java 提交中击败了73.06% 的用户
+     *
+     * @param root 树
+     * @param val 值
+     * @return 新的树
+     */
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (val >= root.val) {
+            TreeNode ans = new TreeNode(val);
+            ans.left = root;
+            return ans;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
     }
 }
