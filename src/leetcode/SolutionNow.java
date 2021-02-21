2aa8dfb109c81ca37a8d8f820fc3ed37c799a733
15927328287@163.com <36834526+PheonixH@users.noreply.github.com>
2021-02-21 15:42:34 +0800

    1438. 绝对差不超过限制的最长连续子数组

--- src/leetcode/SolutionNow.java
+++ src/leetcode/SolutionNow.java
@@ -1422,4 +1422,36 @@ public class SolutionNow {
         }
         return ans;
     }
+
+    /**
+     * 1438. 绝对差不超过限制的最长连续子数组
+     *
+     * 给你一个整数数组 nums ，和一个表示限制的整数 limit，请你返回最长连续子数组的长度，该子数组中的任意两个元素之间的绝对差必须小于或者等于 limit 。
+     * 如果不存在满足条件的子数组，则返回 0 。
+     *
+     * 执行用时：87 ms, 在所有 Java 提交中击败了32.66% 的用户
+     * 内存消耗：47.5 MB, 在所有 Java 提交中击败了82.68% 的用户
+     * @param nums 整数数组
+     * @param limit 限制
+     * @return 绝对差不超过限制的最长连续子数组
+     */
+    public int longestSubarray(int[] nums, int limit) {
+        TreeMap<Integer, Integer> map = new TreeMap<Integer, Integer>();
+        int n = nums.length;
+        int left = 0, right = 0;
+        int ret = 0;
+        while (right < n) {
+            map.put(nums[right], map.getOrDefault(nums[right], 0) + 1);
+            while (map.lastKey() - map.firstKey() > limit) {
+                map.put(nums[left], map.get(nums[left]) - 1);
+                if (map.get(nums[left]) == 0) {
+                    map.remove(nums[left]);
+                }
+                left++;
+            }
+            ret = Math.max(ret, right - left + 1);
+            right++;
+        }
+        return ret;
+    }
 }
